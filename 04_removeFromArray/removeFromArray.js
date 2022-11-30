90f0ce93fde088503b7e71cc9bda802b8e17563d
jflann <26899145+jflann@users.noreply.github.com>
2022-11-30 11:59:45 -0600

    Solve exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  // convert args to array, write filter to return new array
+  const remove = Array.from(args);
+  return arr.filter(item => !(remove.includes(item)) );
 };
 
 // Do not edit below this line
