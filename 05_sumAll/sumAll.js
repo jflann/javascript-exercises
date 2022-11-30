e8b8afc40a1d6e1b0913eee678bb1a730084a129
jflann <26899145+jflann@users.noreply.github.com>
2022-11-30 12:36:23 -0600

    Solve exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+  // reject non-number arguments
+  if (typeof start != 'number' || typeof stop != 'number') return 'ERROR';
+  // reject negative arguments
+  if (start < 0 || stop < 0) return 'ERROR';
+  // sum numbers
+  let result = 0;
+  switch (start < stop) {
+    case true:
+      for (let i = start; i <= stop; i++) {
+        result += i;
+      }
+      return result;
+    case false:
+      for (let i = start; i >= stop; i--) {
+        result += i;
+      }
+      return result;
+  }
 };
 
 // Do not edit below this line
