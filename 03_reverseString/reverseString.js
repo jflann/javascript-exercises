db24c20c53952cb72ec7939516a7beb5928a713d
jflann <26899145+jflann@users.noreply.github.com>
2022-11-30 07:40:19 -0600

    Solve exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const chars = str.split("");
+  let result = '';
+  for (let i = chars.length - 1; i >= 0; i--) {
+    result += chars[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
