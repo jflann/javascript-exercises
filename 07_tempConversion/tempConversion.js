9f71d478a05116c45954f8d1c9d766d57c72b085
jflann <26899145+jflann@users.noreply.github.com>
2022-11-30 18:08:56 -0600

    Solve exercise 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = (fahrenheit - 32) * 5/9;
+  return Number(Math.round(result+'e'+1)+'e-'+1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = (celsius * 9/5 + 32);
+  return Number(Math.round(result+'e'+1)+'e-'+1);
 };
 
 // Do not edit below this line
